686e818bd8bd449432143085c5e71ddb25c0981c
jSY49 <jseung49@gmail.com>
2026-09-11 23:24:40 +0900

    Feat: 임시 로그아웃 추가

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,3 +1,14 @@
+import { useAuth } from '../../app/provider/AuthProvider';
+import Button from '../../shared/ui/button';
+
 export default function HomePage() {
-	return <></>;
+	const { logout } = useAuth();
+
+	return (
+		<main className="p-6">
+			<Button type="button" variant="secondary" onClick={logout}>
+				로그아웃
+			</Button>
+		</main>
+	);
 }

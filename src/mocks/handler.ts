d5d4d58bf0c7f3e2f9e2b32760431ef83ebc4a17
jSY49 <jseung49@gmail.com>
2026-09-15 11:14:48 +0900

    Feat : MSW - 회원 가입 - 회원 가입 요청

--- src/mocks/handler.ts
+++ src/mocks/handler.ts
@@ -47,4 +47,13 @@ export const handlers = [
 	}),
 
 	//회원 가입 - 회원 가입 요청
+	http.post("/api/auth/signup", async ({ request }) => {
+		const body = await request.json();
+		console.log("클라이언트가 보낸 데이터 : ", body);
+		return HttpResponse.json({
+			id: 1,
+			email: "user@example.com",
+			nickname: "닉네임 결과",
+		});
+	}),
 ];

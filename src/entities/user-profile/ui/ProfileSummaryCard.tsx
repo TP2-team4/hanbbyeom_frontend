ce95e5d456fb8d7c78f0ca4f5a17d72215d1fa6b
jSY49 <jseung49@gmail.com>
2026-09-22 09:32:47 +0900

    Fix : 설정 아이콘 위치 변경

--- src/entities/user-profile/ui/ProfileSummaryCard.tsx
+++ src/entities/user-profile/ui/ProfileSummaryCard.tsx
@@ -30,7 +30,7 @@ export function ProfileSummaryCard({ profile }: Props) {
 					<path d="M19.4 15a1.7 1.7 0 0 0 .34 1.87l.06.06a2 2 0 1 1-2.83 2.83l-.06-.06a1.7 1.7 0 0 0-1.87-.34 1.7 1.7 0 0 0-1.04 1.56V21a2 2 0 1 1-4 0v-.09A1.7 1.7 0 0 0 9 19.35a1.7 1.7 0 0 0-1.87.34l-.06.06a2 2 0 1 1-2.83-2.83l.06-.06A1.7 1.7 0 0 0 4.65 15a1.7 1.7 0 0 0-1.56-1.04H3a2 2 0 1 1 0-4h.09A1.7 1.7 0 0 0 4.65 9a1.7 1.7 0 0 0-.34-1.87l-.06-.06a2 2 0 1 1 2.83-2.83l.06.06A1.7 1.7 0 0 0 9 4.65a1.7 1.7 0 0 0 1.04-1.56V3a2 2 0 1 1 4 0v.09A1.7 1.7 0 0 0 15 4.65a1.7 1.7 0 0 0 1.87-.34l.06-.06a2 2 0 1 1 2.83 2.83l-.06.06A1.7 1.7 0 0 0 19.35 9a1.7 1.7 0 0 0 1.56 1.04H21a2 2 0 1 1 0 4h-.09A1.7 1.7 0 0 0 19.4 15Z" />
 				</svg>
 			</button>
-			<div className="flex items-center gap-4">
+			<div className="flex items-center gap-4 pr-8">
 				<span
 					aria-hidden="true"
 					className="grid size-16 shrink-0 place-items-center rounded-full bg-primary-100"
